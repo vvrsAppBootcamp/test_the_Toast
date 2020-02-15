8bb3f895e7224b3ee2ac18c47218882c65fe4b0a
Quantummech2000 <39486314+Quantummech2000@users.noreply.github.com>
2020-02-15 19:31:36 +0530

    :sparkles: Create a Toast and Count app
    Signed-off-by: Quantummech2000 <39486314+Quantummech2000@users.noreply.github.com>

---
+++ app/src/main/java/com/wolf/hworld/MainActivity.java
@@ -0,0 +1,59 @@
+package com.wolf.hworld;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+    private TextView mShowCount;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+
+    }
+
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toastMessage,
+                Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        mCount++;
+        if (mShowCount != null)
+
+            mShowCount.setText(Integer.toString(mCount));
+    }
+
+
+    public void countDown(View view){
+        Button button = (Button) findViewById(R.id.button_countDown);
+        mCount--;
+        if(mShowCount != null)
+
+            mShowCount.setText(Integer.toString(mCount));
+    }
+
+    public void countDown(View view){
+        Button button = (Button) findViewById(R.id.button_Zero);
+        mCount = 0;
+        if(mShowCount != null)
+
+            mShowCount.setText(Integer.toString(mCount));
+    }
+
+
+}
+
